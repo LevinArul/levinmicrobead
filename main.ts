aeaae15615a38b2bc77587fbd413842ede320ddb
LevinArul <86990787+LevinArul@users.noreply.github.com>
2021-07-06 08:19:46 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("LEVIN ARUL ")
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
